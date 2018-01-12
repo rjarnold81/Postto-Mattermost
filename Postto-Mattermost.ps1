bff7196c728ec2cab08115439166b927d1779370
rjarnold81 <rjarnold81@gmail.com>
2018-01-12 08:58:10 -0500

    TLS 1.2/Channel Switch
    Forced the script to communicate over TLS 1.2, and added the channel switch into the JSON payload for the ability to post to a specific channel within Mattermost.

--- Postto-Mattermost.ps1
+++ Postto-Mattermost.ps1
@@ -1,3 +1,6 @@
+#Set the script to use only TLS 1.2 for the duration of the script.
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
 function Postto-Mattermost {
 <#
 .Synopsis
@@ -7,7 +10,7 @@ function Postto-Mattermost {
 .EXAMPLE
    Postto-Mattermost -uri "http://mattermost.foo.com/hooks/xxxxxxxxxxxxxxxxx" -text ”New message from Powershell” -user "Powershell"
 .INPUTS
-   $uri,$text,$user
+   $uri,$text,$user,$channel
 .NOTES
    All Inputs are Mandatory
 #>
@@ -24,8 +27,12 @@ function Postto-Mattermost {
         # Username to post as
         [Parameter(Mandatory=$true
         )]
-        $user
+        $user,
+        # Channel to post within
+        [Parameter(Mandatory=$true
+        )]
+        $channel
      )
-    $Payload = @{ text=$text; username=$user;icon_url=”link to icon”}
+    $Payload = @{ text=$text; channel=$channel; username=$user}
     Invoke-RestMethod -Uri $uri -Method Post -ContentType 'application/json' -Body (ConvertTo-Json $Payload)
 }
